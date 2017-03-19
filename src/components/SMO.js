86b56259bed093d7369395ed7882d8c9cc6d7257
Николай Эмрих <redishko@gmail.com>
2017-03-19 14:24:56 +0300

    Очеред

--- src/components/SMO.js
+++ src/components/SMO.js
@@ -20,25 +20,26 @@ function lol() {
   // for (let i = 0; i < 10; ++i) {
     // Время прибытия ближе, чем время выхода
     if (indicators.nextPopTime === null || indicators.nextArrivalTime < indicators.nextPopTime) {
-      console.log('in')
+      // console.log('in')
       indicators.currentTime = indicators.nextArrivalTime;
       let nextPriority = priority.shift();
       // Если есть свободное устройство
       let freeAttendantIndex = getFreeAttendantIndex(indicators.attendants);
       if (freeAttendantIndex !== false) {
         indicators.attendants[freeAttendantIndex] = true;
+        let popTime = indicators.currentTime + reqProcessingTime.shift();
         indicators.requirementsInProcessing.push({
           attendantIndex: freeAttendantIndex,
           // popTime: indicators.nextPopTime
-          popTime: indicators.currentTime + reqProcessingTime.shift()
+          popTime: popTime
         });
-        attendantsBusyTime[freeAttendantIndex] += indicators.nextPopTime;
+        attendantsBusyTime[freeAttendantIndex] += popTime;
       } else { // Если устройства заняты
         let req = {
           priority: nextPriority,
           processingTime: reqProcessingTime.shift()
         };
-        if (indicators.priorQueue.length < main.I) {
+        if (indicators.priorQueue.length < 2) {
           // Вставляем в очередь на место, идущее за последним таким же приоритетом либо выше
           const indexToInsert = findIndexToInsert(indicators.priorQueue, nextPriority);
           indicators.priorQueue.splice(indexToInsert, 0, req);
@@ -56,11 +57,12 @@ function lol() {
             indicators.priorQueue.splice(indexToInsert, 0, req);
           }
         }
+        ppp.push(indicators.priorQueue.length)
       }
       indicators.nextPopTime = getNextPopTime(indicators.requirementsInProcessing, reqProcessingTime);
       indicators.nextArrivalTime = indicators.currentTime + reqArrivalTime.shift();
     } else { // Время выхода ближе
-      console.log('out')
+      // console.log('out')
       indicators.currentTime = indicators.nextPopTime;
       // Найдем индекс устройства, из которого сейчас должно выйти требование
       let indexToPop = getIndexFromPopTime(indicators.requirementsInProcessing, indicators.nextPopTime);
@@ -73,14 +75,14 @@ function lol() {
           popTime: popTime
         };
         indicators.requirementsInProcessing.splice(indexToPop, 1, req);
-
+        attendantsBusyTime[attIndexToFree] += popTime;
       } else {
         indicators.attendants[attIndexToFree] = null;
         indicators.requirementsInProcessing.splice(indexToPop, 1);
       }
       indicators.nextPopTime = getNextPopTime(indicators.requirementsInProcessing, reqProcessingTime);
     }
-    console.log(indicators, indicators.attendants)
+    // console.log(indicators, indicators.attendants, otkaz)
   }
 }
 
@@ -97,88 +99,9 @@ const getNextPopTime = (requirementsInProcessing, reqPtime) => {
   return min;
 };
 
-let requirements = [];
-function kek() {
-  // for (let i = 0, l = requirements.length; i < l; ++i) {
-  let i = 0
-  while (indicators.nextPopTime) {
-    if (i === 0) {
-      // indicators.nextPopTime = indicators.nextArrivalTime + reqProcessingTime.shift();
-      indicators.nextPopTime = indicators.nextArrivalTime + requirements[0].processingTime;
-      pushToAttendants(requirements[0], indicators.attendants);
-      console.log(indicators, indicators.priorQueue)
-      i = -1
-      continue;
-    }
-    if (indicators.nextArrivalTime < indicators.nextPopTime) {
-      indicators.currentTime = indicators.nextArrivalTime;
-      // if (reqArrivalTime.length)
-      let nextPriority = requirements.shift();
-      nextPriority.processingTime += indicators.currentTime
-      // Если не удается запушить в обработчик - пробуем запушить в очередь
-      if (!pushToAttendants(nextPriority, indicators.attendants)) {
-        // Если очередь не полна
-        if (indicators.priorQueue.length < main.I) {
-          // Вставляем в очередь на место, идущее за последним таким же приоритетом либо выше
-          const indexToInsert = findIndexToInsert(indicators.priorQueue, nextPriority.priority);
-          indicators.priorQueue.splice(indexToInsert, 0, nextPriority);
-          // Задаем время прибытия след.
-        } else { // Очередь полна
-          // Нужен ли отказ?
-          if (isRefuseNeeded(indicators.priorQueue, nextPriority.priority)) {
-            // Отказ нужен
-            otkaz++
-          } else {
-            // Отказ не нужен для следующего приоритета - значит удаляем первый приоритет из очереди, ибо он меньше того, который пришел
-            indicators.priorQueue.shift();
-            // Вставляем в очередь на место, идущее за последним таким же приоритетом либо выше
-            const indexToInsert = findIndexToInsert(indicators.priorQueue, nextPriority.priority);
-            indicators.priorQueue.splice(indexToInsert, 0, nextPriority);
-          }
-        }
-      }
-      indicators.nextArrivalTime = indicators.currentTime + reqArrivalTime.shift();
-
-    } else { // Время заканчивать обслуживание
-      indicators.currentTime = indicators.nextPopTime;
-      // processedRequiremnts.push(indicators.priorQueue.pop());
-      let indexToPopIinAttendants = findReqIndexInAttendants(indicators.nextPopTime, indicators.attendants);
-      console.log(indexToPopIinAttendants)
-      processedRequiremnts.push(indicators.attendants.splice(indexToPopIinAttendants, 1, null));
-      if (indicators.priorQueue.length) {
-        let req = indicators.priorQueue.pop();
-        req.processingTime += indicators.currentTime;
-        pushToAttendants(req, indicators.attendants)
-      }
-        // indicators.nextPopTime = indicators.currentTime + indicators.priorQueue[indicators.priorQueue.length - 1].processingTime;
-      else
-        indicators.nextPopTime = false;
-    }
-    // console.log(indicators, indicators.priorQueue.map(el => el.priority))
-    console.log(indicators.attendants.map(el => el.processingTime), indicators.currentTime)
-  }
-}
-
 const getFreeAttendantIndex = (attendants) => {
   for (let i = 0, l = attendants.length; i < l; ++i) {
     if (!attendants[i]) return i;
-  }
-  return false;
-};
-
-const findReqIndexInAttendants = (popTime, attendants) => {
-  return attendants.findIndex(att => {
-    return att.processingTime === popTime;
-  })
-};
-
-const pushToAttendants = (req, attendants) => {
-  for (let i = 0, l = attendants.length; i < l; ++i) {
-    let att = attendants[i];
-    if (!att) {
-      attendants[i] = req;
-      return i;
-    }
   }
   return false;
 };
@@ -211,7 +134,9 @@ const indicators = {
 };
 let otkaz = 0;
 let processedRequiremnts = [];
+let ppp=[];
 lol()
+console.log(ppp.filter(el => Math.max(...ppp)).length)
 console.log(reqArrivalTime.length, reqProcessingTime.length, priority.length, otkaz);
 
 class SMO extends Component {
